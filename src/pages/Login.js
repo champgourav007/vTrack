9d3fb6c2d6276e544beb5969c8ed12561c0f87e5
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-12-02 14:59:33 +0530

    Azure Authentication

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,24 @@
-import "./Login.css"
+import "./Login.css";
+import { useMsal } from "@azure/msal-react";
+import { loginRequest } from "../config/authConfig";
 
 function Login() {
+  const { instance } = useMsal();
+
+  const handleLogin = (loginType) => {
+    console.log(process.env.REACT_APP_CLIENT_ID);
+    if (loginType === "popup") {
+      instance.loginPopup(loginRequest).catch((e) => {
+        console.log(e);
+      });
+    }
+  };
   return (
     <>
       <span className="Login">
-        <span className="left">
-        </span>
+        <span className="left"></span>
         <div className="right">
-          <button>Login</button>
+          <button onClick={() => handleLogin("popup")}>Login</button>
         </div>
       </span>
     </>
