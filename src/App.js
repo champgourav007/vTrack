fb17691f7f809b848c0782bbc2e4d942889e2560
Gourav Kumar Singh <gouravrajput257@gmail.com>
2022-12-02 02:52:38 -0600

    Added code for login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Vytal</h2>
-    </div>
+    <Login />
   );
 }
 
---
+++ src/pages/Login.js
@@ -0,0 +1,17 @@
+import "./Login.css"
+
+function Login() {
+  return (
+    <>
+      <span className="Login">
+        <span className="left">
+        </span>
+        <div className="right">
+          <button>Login</button>
+        </div>
+      </span>
+    </>
+  );
+}
+
+export default Login;
