8d661f0d1837dede6e95e509fcd48dafaea1c4ce
Gourav Kumar Singh <gouravrajput257@gmail.com>
2022-12-11 23:52:39 -0600

    Added button functionality added

--- src/components/SelectedModule/selected-module.js
+++ src/components/SelectedModule/selected-module.js
@@ -3,10 +3,16 @@ import { filterIcon } from "../../common/icons";
 import './selected-module.css';
 import { DataTable } from './../DataTable/DataTable';
 import TopBar from "../TopBar/TopBar"
+import StickyHeadTable from "../DataTable/Table";
 
 
 export const SelectedModule = ({headingName}) => {
   const [ isEdit, setIsEdit ] = useState(false);
+  const [isAddButtonClicked, setIsAddButtonClicked] = useState(false);
+
+  const addNewRow = (isTrue) => {
+    setIsAddButtonClicked(isTrue)
+  }
 
   return (
     <div className="mainContainer">
@@ -22,11 +28,11 @@ export const SelectedModule = ({headingName}) => {
           <button disable={!isEdit} className={isEdit ? "editBtn" : "disableEditBtn"} onClick={() => { setIsEdit(!isEdit) }}>
             <div className={isEdit ? "btnText" : "disableBtnText" }>Edit Selected</div>
           </button>
-          <button className="addBtn">
-          <div className="btnText">Add</div>
+          <button  disabled={isAddButtonClicked} className={isAddButtonClicked ? "disableAddButton" : "addBtn"} onClick={() => addNewRow(true)}>
+          <div className={ isAddButtonClicked ? "disableAddButtonText" : "btnText"}>Add</div>
           </button>
         </div>
-        <DataTable headingName={headingName} />
+        <DataTable headingName={headingName} isAddButtonClicked={isAddButtonClicked}/>
       </div>
     </div>
   );
